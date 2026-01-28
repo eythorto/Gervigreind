898067654dd7afc08fe0e9f48c07eed2a22d523a
eythorto <eythorto@gmail.com>
2026-01-28 13:24:35 +0000

    lab2 turn in

--- lab2/src/Action.java
+++
@@ -1,5 +0,0 @@
-class Action {
-    private String name;
-    public Action(String name) { this.name = name; }
-    @Override public String toString() { return name; }
-}
--- lab2/src/Orientation.java
+++
@@ -1,1 +0,0 @@
-public enum Orientation {NORTH, EAST, SOUTH, WEST};
