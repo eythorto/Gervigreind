c2846636c2f81bc03b7124a0d6af254ea6faa89f
eythorto <eythorto@gmail.com>
2026-02-04 14:13:14 +0000

    added code for lab4

--- lab4/einstein.py
+++ lab4/einstein.py
@@ -14,39 +14,87 @@ def setup_csp():
     pets = ["Zebra", "Dog", "Fox", "Snails", "Horse"]
 
     # TODO: 1. Create all variables and add them to vars!
- 	# e.g.,
-    # v1 =  model.NewIntVar(1, 5, "variable1")
-    # variables.append(v1)
-    # v2 =  model.NewIntVar(1, 5, "variable2")
-    # variables.append(v2)
-    
+    # e.g.,
+    # Colors, order is "Red", "Green", "Ivory", "Yellow", "Blue" (so c1 is red)
+    c1 = model.NewIntVar(1, 5, colors[0])
+    c2 = model.NewIntVar(1, 5, colors[1])
+    c3 = model.NewIntVar(1, 5, colors[2])
+    c4 = model.NewIntVar(1, 5, colors[3])
+    c5 = model.NewIntVar(1, 5, colors[4])
+
+    # Nations
+    n1 = model.NewIntVar(1, 5, nations[0])
+    n2 = model.NewIntVar(1, 5, nations[1])
+    n3 = model.NewIntVar(1, 5, nations[2])
+    n4 = model.NewIntVar(1, 5, nations[3])
+    n5 = model.NewIntVar(1, 5, nations[4])
+
+    #Cigarettes / Smokes
+    s1 = model.NewIntVar(1, 5, cigarettes[0])
+    s2 = model.NewIntVar(1, 5, cigarettes[1])
+    s3 = model.NewIntVar(1, 5, cigarettes[2])
+    s4 = model.NewIntVar(1, 5, cigarettes[3])
+    s5 = model.NewIntVar(1, 5, cigarettes[4])
+
+    # Drinks
+    d1 = model.NewIntVar(1, 5, drinks[0])
+    d2 = model.NewIntVar(1, 5, drinks[1])
+    d3 = model.NewIntVar(1, 5, drinks[2])
+    d4 = model.NewIntVar(1, 5, drinks[3])
+    d5 = model.NewIntVar(1, 5, drinks[4])
+
+    # Pets
+    p1 = model.NewIntVar(1, 5, pets[0])
+    p2 = model.NewIntVar(1, 5, pets[1])
+    p3 = model.NewIntVar(1, 5, pets[2])
+    p4 = model.NewIntVar(1, 5, pets[3])
+    p5 = model.NewIntVar(1, 5, pets[4])
+
 
     # TODO: 2. Add the constraints to the model!
-    # You might need model.Add(), model.addAbsEquality() and model.AddAllDifferent()
+    # You might need model.Add(), model.AddAbsEquality() and model.AddAllDifferent()
     # see https://developers.google.com/optimization/reference/python/sat/python/cp_model
     # e.g.,
     # model.Add(v1 == v2)
     # model.Add(v1 != v2)
     # model.Add(v1 == v2 + 2)
-    # model.addAbsEquality(2, v1 - v2) # meaning that abs(v1-v2) == 2
+    # model.AddAbsEquality(2, v1 - v2) # meaning that abs(v1-v2) == 2
     # etc.
         
     # The Englishman lives in the red house.
+    model.add(n1 == c1)
     # The Spaniard owns the dog.
+    model.add(n2 == p2)
     # Coffee is drunk in the green house.
+    model.add(d4 == c2)
     # The Ukrainian drinks tea.
+    model.add(n4 == d3)
     # The green house is immediately to the right of the ivory house.
+    model.add(c2 == c3-1)
     # The Old Gold smoker owns snails.
+    model.add(s1 == p4)
     # Kools are smoked in the yellow house.
+    model.add(s2 == c4)
     # Milk is drunk in the middle house.
+    model.add(d5 == 3)
     # The Norwegian lives in the first house.
+    model.add(n3 == 1)
     # The man who smokes Chesterfields lives in the house next to the man with the fox.
+    model.AddAbsEquality(1, s3 - p3)
     # Kools are smoked in the house next to the house where the horse is kept.
+    model.AddAbsEquality(1, s2 - p5)
     # The Lucky Strike smoker drinks orange juice.
+    model.add(s4 == d2)
     # The Japanese smokes Parliaments.
+    model.add(n5 == s5)
     # The Norwegian lives next to the blue house.
+    model.AddAbsEquality(1, n3 - c5)
     # Each of the five houses has a different color, each of the five inhabitants has a different nationality, prefers a different brand of cigarettes, a different drink, and owns a different pet.
-
+    model.add_all_different([s1, s2, s3, s4, s5])
+    model.add_all_different([c1, c2, c3, c4, c5])
+    model.add_all_different([n1, n2, n3, n4, n5])
+    model.add_all_different([d1, d2, d3, d4, d5])
+    model.add_all_different([p1, p2, p3, p4, p5])
     return model, variables
 
 
