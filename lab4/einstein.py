139c9870c1160f160b84285cd058565c2733e3d9
EsjaKS <esja23@ru.is>
2026-02-04 16:48:14 +0000

    final einstein

--- lab4/einstein.py
+++ lab4/einstein.py
@@ -13,44 +13,144 @@ def setup_csp():
     drinks = ["Water", "Orange juice", "Tea", "Coffee", "Milk"]
     pets = ["Zebra", "Dog", "Fox", "Snails", "Horse"]
 
-    # TODO: 1. Create all variables and add them to vars!
- 	# e.g.,
-    # v1 =  model.NewIntVar(1, 5, "variable1")
-    # variables.append(v1)
-    # v2 =  model.NewIntVar(1, 5, "variable2")
-    # variables.append(v2)
-    
+    # House colors
+    Red = model.NewIntVar(1, 5, "Red")
+    variables.append(Red)
 
-    # TODO: 2. Add the constraints to the model!
-    # You might need model.Add(), model.addAbsEquality() and model.AddAllDifferent()
-    # see https://developers.google.com/optimization/reference/python/sat/python/cp_model
-    # e.g.,
-    # model.Add(v1 == v2)
-    # model.Add(v1 != v2)
-    # model.Add(v1 == v2 + 2)
-    # model.addAbsEquality(2, v1 - v2) # meaning that abs(v1-v2) == 2
-    # etc.
-        
+    Blue = model.NewIntVar(1, 5, "Blue")
+    variables.append(Blue)
+
+    Yellow = model.NewIntVar(1, 5, "Yellow")
+    variables.append(Yellow)
+
+    Green = model.NewIntVar(1, 5, "Green")
+    variables.append(Green)
+
+    Ivory = model.NewIntVar(1, 5, "Ivory")
+    variables.append(Ivory)
+
+    # Nationalities
+    Englishman = model.NewIntVar(1, 5, "Englishman")
+    variables.append(Englishman)
+
+    Spaniard = model.NewIntVar(1, 5, "Spaniard")
+    variables.append(Spaniard)
+
+    Norwegian = model.NewIntVar(1, 5, "Norwegian")
+    variables.append(Norwegian)
+
+    Ukrainian = model.NewIntVar(1, 5, "Ukrainian")
+    variables.append(Ukrainian)
+
+    Japanese = model.NewIntVar(1, 5, "Japanese")
+    variables.append(Japanese)
+
+    # Pets
+    Zebra = model.NewIntVar(1, 5, "Zebra")
+    variables.append(Zebra)
+
+    Dog = model.NewIntVar(1, 5, "Dog")
+    variables.append(Dog)
+
+    Fox = model.NewIntVar(1, 5, "Fox")
+    variables.append(Fox)
+
+    Snails = model.NewIntVar(1, 5, "Snails")
+    variables.append(Snails)
+
+    Horse = model.NewIntVar(1, 5, "Horse")
+    variables.append(Horse)
+
+    # Cigarette brand
+    Old_Gold = model.NewIntVar(1, 5, "Old_Gold")
+    variables.append(Old_Gold)
+
+    Kools = model.NewIntVar(1, 5, "Kools")
+    variables.append(Kools)
+
+    Chesterfields = model.NewIntVar(1, 5, "Chesterfields")
+    variables.append(Chesterfields)
+
+    Lucky_Strike = model.NewIntVar(1, 5, "Lucky_Strike")
+    variables.append(Lucky_Strike)
+
+    Parliaments = model.NewIntVar(1, 5, "Parliaments")
+    variables.append(Parliaments)
+
+    # Drinks
+    Water = model.NewIntVar(1, 5, "Water")
+    variables.append(Water)
+
+    Orange_juice = model.NewIntVar(1, 5, "Orange_juice")
+    variables.append(Orange_juice)
+
+    Tea = model.NewIntVar(1, 5, "Tea")
+    variables.append(Tea)
+
+    Coffee = model.NewIntVar(1, 5, "Coffee")
+    variables.append(Coffee)
+
+    Milk = model.NewIntVar(1, 5, "Milk")
+    variables.append(Milk)
+
     # The Englishman lives in the red house.
+    model.Add(Englishman == Red)
+
     # The Spaniard owns the dog.
+    model.Add(Spaniard == Dog)
+
     # Coffee is drunk in the green house.
+    model.Add(Coffee == Green)
+
     # The Ukrainian drinks tea.
+    model.Add(Ukrainian == Tea)
+
     # The green house is immediately to the right of the ivory house.
+    model.Add(Green == Ivory + 1)
+
     # The Old Gold smoker owns snails.
+    model.Add(Old_Gold == Snails)
+
     # Kools are smoked in the yellow house.
+    model.Add(Kools == Yellow)
+
     # Milk is drunk in the middle house.
+    model.Add(Milk == 3)
+
     # The Norwegian lives in the first house.
+    model.Add(Norwegian == 1)
+
     # The man who smokes Chesterfields lives in the house next to the man with the fox.
+    model.AddAbsEquality(
+        1, Chesterfields - Fox
+    )  # Chesterfields can either be on the right/left to the fox house
+
     # Kools are smoked in the house next to the house where the horse is kept.
+    model.AddAbsEquality(
+        1, Kools - Horse
+    )  # Kools can either be on the right/left to the horse house
+
     # The Lucky Strike smoker drinks orange juice.
+    model.Add(Lucky_Strike == Orange_juice)
+
     # The Japanese smokes Parliaments.
+    model.Add(Japanese == Parliaments)
+
     # The Norwegian lives next to the blue house.
+    model.AddAbsEquality(1, Norwegian - Blue)
+
     # Each of the five houses has a different color, each of the five inhabitants has a different nationality, prefers a different brand of cigarettes, a different drink, and owns a different pet.
+    model.AddAllDifferent([Red, Green, Ivory, Yellow, Blue])
+    model.AddAllDifferent([Englishman, Spaniard, Norwegian, Ukrainian, Japanese])
+    model.AddAllDifferent([Old_Gold, Kools, Chesterfields, Lucky_Strike, Parliaments])
+    model.AddAllDifferent([Water, Orange_juice, Tea, Coffee, Milk])
+    model.AddAllDifferent([Zebra, Dog, Fox, Snails, Horse])
 
     return model, variables
 
 
 ##############
+
 
 def solve_csp():
     # create the model
@@ -76,6 +176,7 @@ def solve_csp():
 
 
 ##############
+
 
 def main():
     solve_csp()
